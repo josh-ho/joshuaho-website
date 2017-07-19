6af3c2b777db3c5b4ed374bcdf34808ffa3a9be6
Joshua Ho <jho@innocean.ca>
2017-07-19 10:26:14 -0400

    added redux and basic reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import APP from '../constants'
+import { combineReducers } from 'redux'
+
+function loadWebsiteData( state = [], action ) {
+  switch( action.type ) {
+    case APP.LOADED_DATA :
+      return Object.assign( {}, {
+        data : action.data,
+        lastUpdated : action.lastUpdated
+      });
+  }
+}
+
+const websiteReducer = combineReducers(
+  loadWebsiteData
+)
+
+export default websiteReducer;
